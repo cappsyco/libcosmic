b4b5f51e6936fa3b6c6c78048b750d28b8c22c77
Jonathan Capps <cappsy@gmail.com>
2025-11-27 19:50:28 +0000

    reapplying logo size patch

--- src/applet/mod.rs
+++ src/applet/mod.rs
@@ -181,7 +181,9 @@ impl Context {
 
     #[allow(clippy::cast_precision_loss)]
     pub fn window_settings(&self) -> crate::app::Settings {
-        let (width, height) = self.suggested_size(true);
+        // PATCH: force full size for symbolic icons
+        // let (width, height) = self.suggested_size(false);
+        let (width, height) = self.suggested_size(false);
         let (applet_padding_major_axis, applet_padding_minor_axis) = self.suggested_padding(true);
         let (horizontal_padding, vertical_padding) = if self.is_horizontal() {
             (applet_padding_major_axis, applet_padding_minor_axis)
